10e2bb5105bfdfb3f4b8ba91525e12e6e992c60e
felipemaxplay <felipemaxplay@gmail.com>
2022-06-23 10:55:17 -0300

    config properties created

---
+++ src/config/propsConfig.go
@@ -0,0 +1,56 @@
+package config
+
+import "github.com/spf13/viper"
+
+var (
+	cfg *config
+)
+
+type config struct {
+	Server     ServerConfig
+	DataSource DataSourceConfig
+}
+
+type ServerConfig struct {
+	Port string `json:"port"`
+}
+
+type DataSourceConfig struct {
+	URL string `json:"url"`
+}
+
+func init() {
+	viper.SetDefault("server.port", "8080")
+}
+
+func Load() error {
+	viper.SetConfigName("properties")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./resources/")
+	err := viper.ReadInConfig()
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return err
+		}
+	}
+
+	cfg = new(config)
+
+	cfg.Server = ServerConfig{
+		Port: viper.GetString("server.port"),
+	}
+
+	cfg.DataSource = DataSourceConfig{
+		URL: viper.GetString("datasource.url"),
+	}
+
+	return nil
+}
+
+func GetServer() ServerConfig {
+	return cfg.Server
+}
+
+func GetDataSource() DataSourceConfig {
+	return cfg.DataSource
+}
