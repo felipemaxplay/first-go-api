88a66fbddf32cce85a6d8897ed7420b936a5faaf
felipemaxplay <felipemaxplay@gmail.com>
2022-06-25 11:13:46 -0300

    init main

--- src/main.go
+++ src/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/felipemaxplay/first-go-api/src/config"
+	"github.com/felipemaxplay/first-go-api/src/database"
+	"github.com/felipemaxplay/first-go-api/src/http"
+	"github.com/felipemaxplay/first-go-api/src/repository"
+	"github.com/felipemaxplay/first-go-api/src/server"
+	"github.com/felipemaxplay/first-go-api/src/service"
+	"gorm.io/gorm"
+)
+
+var (
+	errConfig     error                       = config.LoadConfig()
+	db            *gorm.DB                    = database.LoadDB()
+	playerRepo    repository.PlayerRepository = repository.NewPlayerRepository(db)
+	playerService service.PlayerService       = service.NewPlayerService(playerRepo)
+	controller    http.PlayerController       = http.NewPlayerController(playerService)
+	se            server.Server               = server.NewServer(controller)
+)
+
+func main() {
+	if errConfig != nil {
+		log.Println(errConfig)
+	}
+	se.Run()
 }
